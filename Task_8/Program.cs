fb21a68d6a7c7c7c327e6044d745b3f629aee16c
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-10 13:01:57 +0300

    Task 8 done

---
+++ Task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number");
+string example1 = Console.ReadLine();
+int a = Convert.ToInt32(example1);
+
+int i = 0;
+while (i < a)
+{
+    i += 2;
+        if (i <= a) 
+        {
+            Console.Write(i + " ");
+}
+}
