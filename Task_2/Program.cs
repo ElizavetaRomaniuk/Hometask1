aa3234a1894d3d081d2f5b5d9892e8860a57e321
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-10 13:31:40 +0300

    Task 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+string example1 = Console.ReadLine();
+int a = Convert.ToInt32(example1);
+Console.WriteLine("Введите второе число");
+string example2 = Console.ReadLine();
+int b = Convert.ToInt32(example2);
+int maximal = a;
+int minimal = b;
+if (a > b)
+{
+    Console.WriteLine(maximal + " - максимальное, " + minimal + " - минимальное");
+}
+else
+{
+    maximal = b;
+    minimal = a;
+    Console.WriteLine(maximal + " - максимальное, " + minimal + " - минимальное");
+}
+
