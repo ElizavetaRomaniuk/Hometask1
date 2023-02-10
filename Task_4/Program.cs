75f190ee2e57316a6a5bb1647e04dded7b7a2d71
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-10 13:20:49 +0300

    Task 4 done

---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+string example1 = Console.ReadLine();
+int a = Convert.ToInt32(example1);
+Console.WriteLine("Введите второе число");
+string example2 = Console.ReadLine();
+int b = Convert.ToInt32(example2);
+Console.WriteLine("Введите третье число");
+string example3 = Console.ReadLine();
+int c = Convert.ToInt32(example3);
+int maximal = 0;
+if (a > b)
+{
+    maximal = a;
+}
+else
+{
+    maximal = b;
+}
+if (c > maximal)
+{
+    maximal = c;
+}
+Console.WriteLine(maximal);
