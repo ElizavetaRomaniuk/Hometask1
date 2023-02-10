82a09ac73b9c8f793c6c5a8806a27e27274c6e9d
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-10 13:12:28 +0300

    Task 6 done

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+string example1 = Console.ReadLine();
+int a = Convert.ToInt32(example1);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+    
